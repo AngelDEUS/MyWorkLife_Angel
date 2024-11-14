36ca89bd7b94af45efdbf915b9b0eec6bde41956
PC Trabajo Aria_Angel <Aayala@ariapsw.com>
2024-11-14 17:51:02 -0500

    Se crea estructura segun mockup, para fase 2

--- Code_Proyect_Acto_1/Curriculum_front_angel/src/components/Layout/layout.jsx
+++ Code_Proyect_Acto_1/Curriculum_front_angel/src/components/Layout/layout.jsx
@@ -3,24 +3,33 @@ import NavBar from './../NavBar/navBar'
 import MainCurriculum from './../../pages/curriculum/mainCurriculum'
 import "./stylesLayout.css";
 import MainCardForPortfolio from './../../pages/portfolio/mainPortfolio';
+import MainSideMenu from '../SideMenu/mainSideMenu';
 
 const layout = () => {
   return (
     <div className='mainLayout'>
-      <div className="navBarContainer">
+      {/* <div className="navBarContainer">
         <NavBar />
+      </div> */}
+      <div className="sideMenuContainer">
+        <MainSideMenu />
       </div>
       <div className="bodyContainer">
-        <div className="sideContent_layout_left"></div>
+        {/* <div className="sideContent_layout_left"></div> */}
         <div className="contentPage">
           {/* <MainCurriculum /> */}
-          <MainCardForPortfolio/>
+          {/* <MainCardForPortfolio/> */}
         </div>
-        <div className="sideContent_layout_right"></div>
-      </div>
-      <div className="footerMain">
+        <div className="publicity_Slide">
+          <span className="textSlid__Publicity" data-text="50％ 割 引 - 50% de descuento - 50% 할인 - 50% off - 我 们 的 部 分 有 50% 折 扣 - 50％ 割 引 - 50% de descuento - 50% 할인 - 50% off - 我 们 的 部 分 有 50% 折 扣 - 50％ 割 引 - 50% de descuento - 50% 할인 - 50% off - 我 们 的 部 分 有 50% 折 扣 - ">
+            50％ 割 引 - 50% de descuento - 50% 할인 - 50% off - 我 们 的 部 分 有 50% 折 扣 - 50％ 割 引 - 50% de descuento - 50% 할인 - 50% off - 我 们 的 部 分 有 50% 折 扣 - 50％ 割 引 - 50% de descuento - 50% 할인 - 50% off - 我 们 的 部 分 有 50% 折 扣 - 
+          </span>
+        </div>
 
+        {/* <div className="sideContent_layout_right"></div> */}
       </div>
+      {/* <div className="footerMain">
+      </div> */}
     </div>
   )
 }
