d635e135fc3e4f147f890d80e29cf43c78fddcd7
PC Trabajo Aria_Angel <Aayala@ariapsw.com>
2024-11-18 17:46:19 -0500

    Se agrega NavBar para porfolio.

--- Code_Proyect_Acto_1/Curriculum_front_angel/src/components/NavBar/NavbarPorfolio/NavBarPorfolio.jsx
+++ Code_Proyect_Acto_1/Curriculum_front_angel/src/components/NavBar/NavbarPorfolio/NavBarPorfolio.jsx
@@ -2,8 +2,24 @@ import './styleNavBarPorfolio.css'
 
 const NavBarPorfolio = () => {
   return (
-    <div>
-      xxD
+    <div className='mainNavBarPorfolio'>
+      <div className="containerButtons">
+        <div className="buttonCategory_NavBar">
+          <span className='categoryText'>All projects web</span>
+        </div>
+        <div className="buttonCategory_NavBar">
+          <span className='categoryText'>Projects mobile</span>
+        </div>
+        <div className="buttonCategory_NavBar">
+          <span className='categoryText'>Web UI/UX</span>
+        </div>
+        <div className="buttonCategory_NavBar">
+          <span className='categoryText'>Illustration</span>
+        </div>
+        <div className="buttonCategory_NavBar">
+          <span className='categoryText'>Socialmedia Design</span>
+        </div>
+      </div>
     </div>
   )
 }
--- Code_Proyect_Acto_1/Curriculum_front_angel/src/pages/portfolio/mainPortfolio.jsx
+++ Code_Proyect_Acto_1/Curriculum_front_angel/src/pages/portfolio/mainPortfolio.jsx
@@ -1,8 +1,12 @@
 import CardP_Best from '../../components/Cards/CardP_Best'
+import NavBarPorfolio from '../../components/NavBar/NavbarPorfolio/NavBarPorfolio'
 
 const MainPortfolio = () => {
   return (
-    <div className='mainPorfolio_Page' style={{ display: 'flex'}}>
+    <div className='mainPorfolio_Page' style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className="navBar__Portfolio">
+        <NavBarPorfolio />
+      </div>
       <div className="contentMainPorfolio_page" style={{ display: 'flex', padding: '30px', gap: '30px' }}>
         <CardP_Best />
         <CardP_Best />
