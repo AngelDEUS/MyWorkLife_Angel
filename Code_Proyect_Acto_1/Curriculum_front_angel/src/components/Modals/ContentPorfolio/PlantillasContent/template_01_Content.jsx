051f72eba71b1d8d1d7d21aaca992a00c7c99317
PC Trabajo Aria_Angel <Aayala@ariapsw.com>
2024-12-09 17:49:13 -0500

    Se realizan ajuste en el contenido del modal

---
+++ Code_Proyect_Acto_1/Curriculum_front_angel/src/components/Modals/ContentPorfolio/PlantillasContent/template_01_Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import IMGImportTest from './../../../../../public/assets/potography/'
+
+const Template_01_Content = () => {
+  return (
+    <div className='mainTemplate_01_Porfolio'>
+      <div className="imgContainer_Style_S1">
+        <div className="degradedImgTemplate" />
+        <img src="" alt="imagenTemplate.png" className="imgTemplate_S1" />
+      </div>
+    </div>
+  )
+}
+
+export default Template_01_Content
--- Code_Proyect_Acto_1/Curriculum_front_angel/src/components/Modals/ContentPorfolio/mainContentPortfolio.jsx
+++ Code_Proyect_Acto_1/Curriculum_front_angel/src/components/Modals/ContentPorfolio/mainContentPortfolio.jsx
@@ -5,7 +5,7 @@ const MainContentPortfolio = () => {
     return (
         <div className='mainContainer__ContentPortfolio'>
             <div className="headerAlwaysTOP">
-                <HeaderPortfolio/>
+                <HeaderPortfolio />
             </div>
             <div className="content_TopPortfolio">
                 <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
@@ -17,29 +17,12 @@ const MainContentPortfolio = () => {
                 <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
                 <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
             </div>
-            <div className="content_TopPortfolio">
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-            </div>
-            <div className="content_TopPortfolio">
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem commodi ipsa quae repudiandae molestias vitae quidem temporibus eum beatae. Iure, commodi consectetur quisquam excepturi laudantium ducimus ea asperiores optio accusamus?</span>
-            </div>
-            <div className="content_MiddlePortfolio">
-            </div>
-            <div className="content_BottomPortfolio">
+            <div className="galleryDivider" />
+            <div className="content_BottomPortfolio__Galery">
+                <h3 className="tittleGallery_S1">Galeria de Imagenes <span className="tagColor__S1">(Interfaz)</span></h3>
+                <p className="paragraph_gallery_S1">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe assumenda praesentium ab provident veniam excepturi doloribus exercitationem debitis quos at?
+                </p>
             </div>
         </div>
     )
